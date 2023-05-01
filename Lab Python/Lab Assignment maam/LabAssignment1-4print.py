143858c246ae6b4ed2e8ef545f3a1b9e71069755
Rahul Kumar <kumarrahul771996@gmail.com>
2023-05-01 11:51:42 +0530

    Update LabAssignment1-4print.py
    converted to procedural approach

--- Lab Python/Lab Assignment maam/LabAssignment1-4print.py
+++ Lab Python/Lab Assignment maam/LabAssignment1-4print.py
@@ -1,27 +1,28 @@
-def ten_natural_nos():
-    print("\nFirst ten Natural numbers")
-    for nos in range(1,11):
+# print first ten natural numbers
+print("\nFirst ten Natural numbers")
+for nos in range(1,11):
         print(nos, end = " ")
 
 
 
 
-def ten_even_nos():
-    print("\n\nTen Even numbers in reverse")
-    for nos in range(20, 1, -2):
+# print first ten even numbers
+print("\n\nTen Even numbers in reverse")
+for nos in range(20, 1, -2):
         print(nos, end = " ")
 
 
 
 
-def table_of_no():
-    num = int(input("\n\nEnter number to get table: "))
-    for i in range(1,11):
+# print table of given number
+num = int(input("\n\nEnter number to get table: "))
+for i in range(1,11):
         print(num * i)
 
 
 
-              
+
+# print first ten prime numbers
 def is_prime(num):
     true_or_false = True
     
@@ -33,16 +34,8 @@ def is_prime(num):
             true_or_false = False
     return true_or_false
 
-def ten_prime_nos():
-    print("\nFirst ten Prime numbers")
-    for no in range(2, 30):
+print("\nFirst ten Prime numbers")
+for no in range(2, 30):
         if is_prime(no):
-            print(no, end = " ")
+                print(no, end = " ")
 
-
-
-            
-ten_natural_nos()
-ten_even_nos()
-table_of_no()
-ten_prime_nos()
