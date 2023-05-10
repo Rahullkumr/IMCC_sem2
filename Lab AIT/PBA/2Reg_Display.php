da7cf686c0096eb3486312a20d79ba2987241e28
Rahul Kumar <kumarrahul771996@gmail.com>
2023-05-10 22:34:41 +0530

    Update 2Reg_Display.php
    php qn2 edited

--- Lab AIT/PBA/2Reg_Display.php
+++ Lab AIT/PBA/2Reg_Display.php
@@ -5,13 +5,13 @@
         <style>
             form{
                 border: 2px solid red;
-                width: 300px;   height: 410px;
+                width: 300px;   height: 510px;
                 background-color: lightblue;
             }
             .submit{                
                 font-weight: bold;
             }
-            input{
+            select, input{
                 height: 25px;   width: 60%;
             }
         </style>
@@ -24,9 +24,16 @@
             <h2>Student Registration</h2>
             <input type="text" name="name" placeholder="Your Name" required><br><br>
             <input type="email" name="email" placeholder="Email Address" required><br><br>
+            Birthday: <br><input type="date" name="dob" required><br><br>
+            Course: <br>
+            <select name="course">
+                <option>-- Select --</option>
+                <option name="course" value="mba">MBA</option>
+                <option name="course" value="mca">MCA</option>
+            </select><br><br>
             <input type="number" name="number" placeholder="Whatsapp number" required><br><br>
-            <textarea name="address" cols="22" rows="5" placeholder="Present Address" required></textarea><br><br>
-            <input type="password" name="password" placeholder="8 Characters Password" required><br><br>
+            <textarea name="address" cols="22" rows="4" placeholder="Present Address" required></textarea><br><br>
+            <input type="password" name="password" placeholder="Password" required><br><br>
             <input class="submit" type="submit" name="SubmitClicked"><br>
         </form>
     </body>
