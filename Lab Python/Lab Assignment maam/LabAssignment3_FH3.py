21b84d1435009511a23e15806aeff75afea86f5e
Rahul Kumar <kumarrahul771996@gmail.com>
2023-06-03 18:37:32 +0530

    Update LabAssignment3_FH3.py
    fh3 done

--- Lab Python/Lab Assignment maam/LabAssignment3_FH3.py
+++ Lab Python/Lab Assignment maam/LabAssignment3_FH3.py
@@ -1,18 +1,15 @@
 # 3. Write Python program to Get number of characters, words, spaces and lines in a file
 
-char_count = 0
-word_count = 0
-space_count = 0
-line_count = 0
+char_count, word_count, space_count, line_count = 0, 0, 0, 0
 
 with open("questions.txt") as f:
-    for line in f:
-        line_count += 1
-
-
-print(char_count)
-print(word_count)
-print(space_count)
-print(line_count)
-
-adhuraaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    content = f.read()
+    char_count = len(content)
+    word_count = content.split()
+    space_count = content.count(' ')
+    line_count = content.count('\n') + 1
+    
+    print(char_count)
+    print(word_count)
+    print(space_count)
+    print(line_count)
