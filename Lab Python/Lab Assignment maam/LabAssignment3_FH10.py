fea7c696bf3adfe9b8c40a87c0a0bd36d43129b6
Rahul Kumar <kumarrahul771996@gmail.com>
2023-06-03 16:42:00 +0530

    Create LabAssignment3_FH10.py
    fh 10 done

---
+++ Lab Python/Lab Assignment maam/LabAssignment3_FH10.py
@@ -0,0 +1,10 @@
+with open("file1.txt") as file1:
+    f1_content = file1.read()
+
+with open("file2.txt") as file2:
+    f2_content = file2.read()
+
+with open("mergedFile.txt", "x") as file3:
+    file3.write(f1_content)
+    file3.write("\n")
+    file3.write(f2_content)
